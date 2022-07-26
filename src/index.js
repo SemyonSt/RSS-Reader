f52bc02a5a5494dacfbf5cef57784092aef9911b
Semyon <sema.stulnikov@mail.ru>
2022-07-27 01:08:59 +0300

    4

--- src/index.js
+++ src/index.js
@@ -1,3 +1,52 @@
-import 'bootstrap';
+//import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-console.log('Hello')
+import * as yup from 'yup';
+import onChange from 'on-change';
+
+let schema = yup.object().shape({
+    website: yup.string().url()
+});
+/*
+const state = onChange({
+    form: {
+        valid: true,
+        processState: 'filling',
+        processError: null,
+    }
+});
+*/
+const errorMessages = {
+    duplicateLink: 'RSS уже существует',
+    fieldRequired: 'Не должно быть пустым',
+    invalidURL: 'Ссылка должна быть валидным URL',
+};
+
+
+const validate = (fields) => {
+    try {
+        schema.validateSync(
+            { website: fields },
+            { abortEarly: false },
+          );
+      return 'Valid';
+    } catch (e) {
+      return 'error'
+    }
+  };
+
+const elements ={
+    form: document.querySelector('.rss-form'),
+    input: document.querySelector('#url-input'),
+    btn: document.querySelector('.h-100 '),
+    feedBack: document.querySelector('.feedback'),
+}
+
+//console.log(elements.input);
+
+elements.input.addEventListener('submit', (e) => {
+    e.preventDefault()
+    console.log(input.value)
+    // if(validate(121) !== "Valid") {
+    //     e.textContent = errorMessages.invalidURL
+    // }
+})
--- webpack.config.js
+++ webpack.config.js
@@ -20,10 +20,7 @@ module.exports = {
             },
           },
           { test: /\.css$/, use: ['style-loader', 'css-loader', 'postcss-loader'] },
-          {
-            test: /\.scss$/,
-            use: ['style-loader', 'css-loader', 'sass-loader', 'postcss-loader'],
-          },
+        
           {
             test: /\.woff2?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
             use: 'url-loader?limit=10000',
