50238a78f4b5f9560c3a10b64c84b03811c9423e
Семён <sema.stulnikov@mail.ru>
2022-09-28 21:56:35 +0300

    step3.6

--- src/app.js
+++ src/app.js
@@ -1,71 +1,13 @@
 import onChange from 'on-change';
 import i18next from 'i18next';
 import * as _ from 'lodash';
-import { successInput, dangerInput, invalidRSS, posts } from './view';
+import {
+  successInput, dangerInput, invalidRSS, posts,
+} from './view';
 import validate from './controller';
 import ru from './locales/index';
 
-const parse = (data) => {
-  const parser = new DOMParser();
-  const dom = parser.parseFromString(data, 'application/xml');
-
-  const feedName = {
-    title: dom.querySelector('title').innerHTML,
-    description: dom.querySelector('description').innerHTML,
-  };
-
-  const domItem = dom.querySelectorAll('item');
-  const feedPosts = Array.from(domItem).map((item) => {
-    const titles = item.querySelector('title').innerHTML;
-    const links = item.querySelector('link').innerHTML;
-    const descriptions = item.querySelector('description').innerHTML;
-    return { titles, links, descriptions };
-  });
-  return { feedName, feedPosts };
-};
-
 const runApp = async () => {
-  const uniq = (arr) => {
-    const seen = {};
-    return arr.filter((x) => {
-      const key = JSON.stringify(x);
-      return !(key in seen) && (seen[key] = x);
-    });
-  };
-
-  const getRss = (url, state) => {
-    // console.log(uniq(state.posts))
-    fetch(`https://allorigins.hexlet.app/get?url=${encodeURIComponent(url)}`)
-      .then((response) => {
-        if (response.ok) return response.json();
-        throw new Error('Network response was not ok.');
-      })
-      .then((data) => {
-        if (data.contents === null) {
-          const error = new Error('Ресурс не содержит валидный RSS');
-          state.form.valid = 'lol';
-          throw error;
-        }
-        // console.log(data.contents;
-        return data.contents;
-      })
-      .catch((error) => {
-        console.log(error);
-        state.message.message= error;
-        console.log('1FFFFFFFASD', state.message.message)
-      })
-      .then((xml) => {
-        parse(xml).feedPosts.map((i) => state.posts.push(i));
-        Object.assign(state.postsName, parse(xml).feedName);
-        return uniq(state.posts);
-      })
-      .then((q) => {
-        if (state.form.valid === true) {
-          posts(state);
-        }
-      });
-  };
-
   const state = {
     form: {
       valid: true,
@@ -93,7 +35,7 @@ const runApp = async () => {
     resources: { ru },
   }).then(() => {
     const watchedState = onChange(state, () => {
-      console.log('STAAAAAAAATE', state)
+      // console.log('STAAAAAAAATE', state)
       if (state.form.valid === true) {
         return successInput(state);
       }
@@ -103,8 +45,9 @@ const runApp = async () => {
       e.preventDefault();
       const data = new FormData(e.target);
       const url = data.get('url').trim();
-      validate(i18next, watchedState, url);
-      getRss(url, state);
+      validate(i18next, watchedState, url, state);
+      // console.log(validate(i18next, watchedState, url));
+      // getRss(url, state);
     });
   });
 };
--- src/controller.js
+++ src/controller.js
@@ -1,6 +1,62 @@
 import * as yup from 'yup';
+import { posts } from './view';
 
-const validate = async (i18n, state, url) => {
+const parse = (data) => {
+  const parser = new DOMParser();
+  const dom = parser.parseFromString(data, 'application/xml');
+
+  const feedName = {
+    title: dom.querySelector('title').innerHTML,
+    description: dom.querySelector('description').innerHTML,
+  };
+
+  const domItem = dom.querySelectorAll('item');
+  const feedPosts = Array.from(domItem).map((item) => {
+    const titles = item.querySelector('title').innerHTML;
+    const links = item.querySelector('link').innerHTML;
+    const descriptions = item.querySelector('description').innerHTML;
+    return { titles, links, descriptions };
+  });
+  return { feedName, feedPosts };
+};
+const uniq = (arr) => {
+  const seen = {};
+  return arr.filter((x) => {
+    const key = JSON.stringify(x);
+    return !(key in seen) && (seen[key] = x);
+  });
+};
+const getRss = (url, state) => {
+  // console.log(uniq(state.posts))
+  fetch(`https://allorigins.hexlet.app/get?url=${encodeURIComponent(url)}`)
+    .then((response) => {
+      if (response.ok) return response.json();
+      throw new Error('Network response was not ok.');
+    })
+    .then((data) => {
+      if (data.contents === null) {
+        const error = new Error('Ресурс не содержит валидный RSS');
+        throw error;
+      }
+      // console.log(data.contents);
+      return data.contents;
+    })
+
+    .then((xml) => {
+      parse(xml).feedPosts.map((i) => state.posts.push(i));
+      Object.assign(state.postsName, parse(xml).feedName);
+      // return uniq(state.posts);
+    })
+    .then((q) => posts(state));
+  // .catch((error) => {
+  //   console.log(error);
+  //   state.form.valid = false;
+  //   state.message = error.message;
+  //   console.log('1FFFFFFFASD', state.message.message, error.message);
+  // });
+};
+
+const validate = async (i18n, watchedState, url, state) => {
   yup.setLocale({
     mixed: {
       notOneOf: i18n.t('notValidDouble'),
@@ -12,15 +68,17 @@ const validate = async (i18n, state, url) => {
 
   const schema = yup.string().url().required();
 
-  schema.notOneOf(state.form.data).validate(url).then(() => {
-    state.form.valid = true;
-    state.form.data.push(url);
-    state.message = i18n.t('validRss');
-  })
+  schema.notOneOf(watchedState.form.data).validate(url)
+    .then(() => {
+      watchedState.form.valid = true;
+      watchedState.form.data.push(url);
+      watchedState.message = i18n.t('validRss');
+      getRss(url, state);
+    })
     .catch((err) => {
       console.log(err);
-      state.form.valid = false;
-      state.message = err.errors;
+      watchedState.form.valid = false;
+      watchedState.message = err.message;
     });
 };
 
